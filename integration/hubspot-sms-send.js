d903d398730336b519a3c89116cbac95f50eb58b
Dan Wong <dawong@twilio.com>
2022-04-12 15:51:35 +1000

    hubspot SMS send

---
+++ integration/hubspot-sms-send.js
@@ -0,0 +1,53 @@
+/***
+ * This code snippit can be used in Hubspot Workflows to send SMS. It calls Twilio API directly to send a single SMS.
+ * 
+ * Setup
+ * 
+ * - Add Secrets to Workflow
+ *   - AccountSID from your Twilio Console
+ *   - AuthToken from your Twilio Console
+ * - Add Properties to Workflow
+ *   - mobilephone from Hubspot Contact's Mobile Phone Number
+ * 
+ * - Edit the phoneNumber and body of the message as required. 
+ */
+
+const hubspot = require('@hubspot/api-client');
+const axios = require('axios')
+
+exports.main = async (event, callback) => {
+
+  const accountSID = process.env.AccountSID
+  const authToken = process.env.AuthToken
+  const toPhoneNumber = event.inputFields['mobilephone']
+  const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSID}/Messages.json`
+  
+  // Change this to suit your needs, you can pass a template for text body. 
+  const phoneNumber = '+61480018956'
+  const body = `Hi ${event.inputFields['firstname']}, Test Message`
+  
+  // Required Parameters
+  const params = new URLSearchParams()
+  params.append('From', phoneNumber)
+  params.append('To', toPhoneNumber)
+  params.append('Body', body)
+
+  // Required Headers
+  const config = {
+    headers: {
+      'Authorization': 'Basic '+ Buffer.from(`${accountSID}:${authToken}`).toString('base64'), 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  const response = await axios.post(url, params, config)
+
+  // you may want to have some error handling here. 
+  console.log(response.data)
+  
+  callback({
+    outputFields: {
+      MessageSid: response.data.sid
+    }
+  });
+}
